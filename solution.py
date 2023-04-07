90b3ef1c5db4a43b36535826dde691b194206656
AlexIGN <84693562+AlexeyIGN@users.noreply.github.com>
2023-04-08 02:59:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,16 +2,22 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
+from scipy.stats import dlaplace
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1017890038 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    loc = np.median(x)
+    temp = []
+    for i in len(x):
+        temp[i] = x[i] - loc
+    D = 2*(np.sum(temp))**2
+    D = D/len(x)
+    D = D/len(x)
+    scale = np.sqrt(D) / np.sqrt(len(x))
+    return loc - scale * dlaplace.ppf(1 - alpha / 2), \
+           loc - scale * dlaplace.ppf(alpha / 2)
