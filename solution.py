187db3c126f0cf0a6425c8291f7a356def3de084
AlexIGN <84693562+AlexeyIGN@users.noreply.github.com>
2023-04-08 04:27:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,24 +1,19 @@
-import pandas as pd
-import numpy as np
+import pandas as pd 
+import numpy as np 
+from scipy.stats import norm 
 
-from scipy.stats import norm
-from scipy.stats import laplace
+chat_id = 1017890038 
 
-chat_id = 1017890038 # Ваш chat ID, не меняйте название переменной
-
-def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = np.median(x)
-    temp = np.zeros(len(x))
-    for i in range(len(x)):
-        temp[i] = x[i] - loc
-    #D = 2*(np.sum(temp))**2
-    #D = D/len(x)
-    #D = D/len(x)
-    #scale = np.sqrt(D) / np.sqrt(len(x))
-    scale = np.sqrt(2)*np.sum(temp) / np.sqrt(len(x))
-    return (2 / 33**2)*loc - (2 / 33**2)*scale * laplace.ppf(1 - alpha / 2), \
-           (2 / 33**2)*loc - (2 / 33**2)*scale * laplace.ppf(alpha / 2)
+def solution(p: float, x: np.array) -> tuple: 
+    E=x.mean() 
+    s=0
+    for c in x: 
+        s+=(c-E)**2 
+    s/=len(x)-1 
+    s=s**0.5 
+    alpha=1-p 
+    left = 0.5 + E + norm.ppf(alpha/2)*s/len(x)**0.5 
+    right = 0.5 + E - norm.ppf(alpha/2)*s/len(x)**0.5
+    left*=2/2**2 
+    right*=2/2**2 
+    return left, right
