0ac1c64051896473e9f79fc388d0ae4e64937ab7
AlexIGN <84693562+AlexeyIGN@users.noreply.github.com>
2023-04-08 05:24:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
 from scipy.stats import norm
+from scipy.stats import chi2
 
 chat_id = 1017890038 
 
@@ -12,8 +13,10 @@ def solution(p: float, x: np.array) -> tuple:
     s/=len(x)-1
     s=s**0.5 
     alpha=1-p 
-    left = 0.5 + E + norm.ppf(alpha/2)*s/len(x)**0.5 
-    right = 0.5 + E - norm.ppf(alpha/2)*s/len(x)**0.5
+    #left = 0.5 + E + chi2.ppf(alpha/2)*s/len(x)**0.5 
+    #right = 0.5 + E - chi2.ppf(alpha/2)*s/len(x)**0.5
+    left = 0.5 - E - 2*len(x)*E / chi2.ppf((1+p)/2 , df = 2*len(x))
+    right = 0.5 - E - 2*len(x)*E / chi2.ppf((1-p)/2 , df = 2*len(x))
     left*=2/2**2 
     right*=2/2**2 
     return left, right
