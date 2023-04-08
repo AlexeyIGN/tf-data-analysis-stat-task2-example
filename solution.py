f10741882f47333df9c56d2517b183a149f64ef9
AlexIGN <84693562+AlexeyIGN@users.noreply.github.com>
2023-04-08 04:37:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
 from scipy.stats import norm 
+from scipy.stats import expon
 
 chat_id = 1017890038 
 
@@ -9,11 +10,11 @@ def solution(p: float, x: np.array) -> tuple:
     s=0
     for c in x: 
         s+=(c-E)**2 
-    s/=len(x) 
+    s/=len(x) - 1
     s=s**0.5 
     alpha=1-p 
-    left = 0.5 + E + norm.ppf(alpha/2)*s/len(x)**0.5 
-    right = 0.5 + E - norm.ppf(alpha/2)*s/len(x)**0.5
+    left = 0.5 + E + expon.ppf(alpha/2)*s/len(x)**0.5 
+    right = 0.5 + E - expon.ppf(alpha/2)*s/len(x)**0.5
     left*=2/2**2 
     right*=2/2**2 
     return left, right
